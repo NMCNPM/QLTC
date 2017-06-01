55c5c97dc814ac9ace26d75a61361e90e707ab87
DaoTienPhat <phat4everduke@gmail.com>
2017-06-01 22:07:02 +0700

    change DatabaseData

--- CMNNPM/DatabaseData.cs
+++ CMNNPM/DatabaseData.cs
@@ -42,13 +42,27 @@ namespace CMNNPM
         private static DataTable DanhSachTiecCuoi;
         private DataSet mSetDichVu;
 
-
-        public bool queryTableDichVu(String cmd)
+        public string generateID(string Header)
         {
-            mComm = new OleDbCommand(cmd, mConn);
-            DichVu = new DataTable();
-            DichVu.Load(mComm.ExecuteReader());
-            return true;
+            string result = DateTime.Now.ToString("ddMMyyyyhhmmss");
+            result = Header + result;
+            return result;
+        }
+
+        public DataTable queryTable(String cmd)
+        {
+            DataTable mTable = new DataTable();
+
+            SqlConnection khConnection = new SqlConnection();
+            khConnection.ConnectionString = CONNECTION_STRING;
+            khConnection.Open();
+
+            SqlCommand khCommand = khConnection.CreateCommand();
+            khCommand.CommandText = cmd;
+
+            mTable.Load(khCommand.ExecuteReader());
+            khConnection.Close();
+            return mTable;
         }
 
         private void createConnection()
@@ -165,29 +179,107 @@ namespace CMNNPM
 
         public bool insertKhachHang(String tenCoDau, String tenChuRe, String SDT)
         {
-            SqlConnection khConnection = new SqlConnection();
-            khConnection.ConnectionString = CONNECTION_STRING;
-            khConnection.Open();
-
-            SqlCommand khCommand = khConnection.CreateCommand();
-            khCommand.CommandText = "INSERT INTO KHACHHANG VALUES('" + 
-                1212121 + "','" +
+            DataTable tablet = queryTable("INSERT INTO KHACHHANG VALUES('" + 
+                generateID("KH") + "','" +
                 tenCoDau +"','" +
                 tenChuRe + "', '"+
-                SDT +"');";
-
-            khCommand.ExecuteNonQuery();
-            khConnection.Close();
+                SDT +"');");
+           
             return true;
         }
 
-        public bool insertTiecCuoi(String tenChuRe, String tenCoDau, String SDT,
+        public DataTable loadTenLoaiSanh()
+        {            
+            return queryTable("SELECT LOAISANH.TENLOAISANH FROM LOAISANH");
+        }
+
+        public DataTable loadCa()
+        {
+            return queryTable("SELECT CA.TENCA FROM CA");
+        }
+
+        public DataTable loadLoaiSanh()
+        {
+            return queryTable("SELECT * FROM LOAISANH");
+        }
+
+        public DataTable loadSanh()
+        {
+            return queryTable("SELECT * FROM SANH");
+        }
+        public DataTable loadTenSanh()
+        {
+            return queryTable("SELECT SANH.TENSANH FROM SANH");
+        }
+
+        public DataTable loadSLBanToiDa()
+        {
+            return queryTable("SELECT SANH.SLBANTOIDA FROM SANH");
+        }
+
+        public String selectMaCaFromTenCa(String tenCa)
+        {
+            String maCa = "";
+            DataTable caTable = queryTable("SELECT CA.MACA FROM CA "
+                + "WHERE CA.TENCA = '" + tenCa
+                + "' ORDER BY CA.MACA DESC");
+
+            maCa = caTable.Rows[0][0].ToString();
+            return maCa;
+        }
+
+        public String selectMaKhachHangFromCoDauChuRe(String tenCoDau, String tenChuRe)
+        {
+            String ma = "";
+            DataTable mTable = queryTable("SELECT KHACHHANG.MAKHACHHANG FROM KHACHHANG "
+                + "WHERE KHACHHANG.TENCODAU = '" + tenCoDau
+                + "' AND KHACHHANG.TENCHURE = '" + tenChuRe
+                + "' ORDER BY KHACHHANG.MAKHACHHANG DESC");
+
+            ma = mTable.Rows[0][0].ToString();
+            return ma;
+        }
+
+        public String selectMaSanhFromTenSanh(String tenSanh)
+        {
+            String ma = "";
+            DataTable mTable = queryTable("SELECT SANH.MASANH FROM SANH "
+                + "WHERE SANH.TENSANH = '" + tenSanh
+                + "' ORDER BY SANH.MASANH DESC");
+
+            ma = mTable.Rows[0][0].ToString();
+            return ma;
+        }
+
+        public bool insertDatTiec(String tenChuRe, String tenCoDau, String SDT,
+            DateTime ngayThang, String loaiSanh, String sanh, String ca,
+            String slBan, String slBanDuTru)
+        {
+            insertKhachHang(tenCoDau, tenChuRe, SDT);
+            DataTable khachhangTable = queryTable("SELECT KHACHHANG.MAKHACHHANG FROM KHACHHANG WHERE "
+                + "TENCODAU = '" + tenCoDau + "'AND "
+                + "TENCHURE = '" + tenChuRe + "' ORDER BY KHACHHANG.MAKHACHHANG ASC;");
+
+            DataTable tablet = queryTable("INSERT INTO TIECCUOI VALUES('" +
+                generateID("TC") + "','" +
+                selectMaKhachHangFromCoDauChuRe(tenCoDau, tenChuRe).Trim() + "','" +
+                selectMaSanhFromTenSanh(sanh).Trim() + "', '" +
+                selectMaCaFromTenCa(ca).Trim() + "', '" +
+                ngayThang + "', '" +
+                slBan + "', '" +
+                slBanDuTru + "');");
+
+            return true;
+        }
+
+        public bool alterDatTiec(String tenChuRe, String tenCoDau, String SDT,
             DateTime ngayThang, String loaiSanh, String sanh, String ca,
             String slBan, String slBanDuTru)
         {
 
             return true;
         }
+
         public void close()
         {
             mConn.Close();
